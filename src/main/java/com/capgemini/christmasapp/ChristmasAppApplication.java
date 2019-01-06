936c883d814db92044db09fac2c2712cc5431710
Vos <stein.vos@capgemini.com>
2019-01-06 15:19:24 +0100

    initial commit

---
+++ src/main/java/com/capgemini/christmasapp/ChristmasAppApplication.java
@@ -0,0 +1,14 @@
+package com.capgemini.christmasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristmasAppApplication.class, args);
+	}
+
+}
+
